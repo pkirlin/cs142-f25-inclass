36a3f46e31ca625fdab19ce9b9b392dbece4b1dc
Phil Kirlin <kirlinp@rhodes.edu>
2025-09-03 10:11:30 -0500

    s

--- src/functions/FunctionPractice.java
+++ src/functions/FunctionPractice.java
@@ -6,6 +6,23 @@ public class FunctionPractice {
         return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
     }
 
+    public static int sumRange(int lower, int upper) {
+        int sum = 0;
+        for (int ctr = lower; ctr <= upper; ctr++) {
+            sum = sum + ctr; // or sum += ctr
+        }
+        return sum;
+    }
+
+    public static double totalDistance(int[] points) {
+        double sum = 0;
+        for (int ctr = 0; ctr < points.length - 2; ctr += 2) {
+            double dist = distance(points[ctr], points[ctr+1], points[ctr+2], points[ctr+3]);
+            sum += dist;
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
         // Write code here to test each function:
 
@@ -13,7 +30,11 @@ public class FunctionPractice {
         System.out.println(distance(1, 2, 3, 4)); // should be about 2.828
         System.out.println(distance(3, 7, -5, 1));  // should be 10.0
 
+        // test sumRange
+        System.out.println(sumRange(1, 9));
+
         // Here is an array you can use for the totalDistance() function:
         int[] array = new int[] { 1, 2, 3, -4, -3, 0 };
+        System.out.println(totalDistance(array));
     }
 }
