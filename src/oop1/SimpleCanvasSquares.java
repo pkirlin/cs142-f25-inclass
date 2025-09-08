0e087a1b4020d04ac0d2b3fed45ac7a5dc0df3a8
Phil Kirlin <kirlinp@rhodes.edu>
2025-09-08 10:28:00 -0500

    updates

--- src/oop1/SimpleCanvasSquares.java
+++ src/oop1/SimpleCanvasSquares.java
@@ -9,5 +9,11 @@ public class SimpleCanvasSquares {
         // a concentric square design similar to this:
         // https://www.artsy.net/artwork/frank-stella-gran-cairo-4
 
+        SimpleCanvas canvas = new SimpleCanvas(500, 500);
+
+        canvas.drawFilledRectangle(100, 100, 300, 300);
+        canvas.setPenColor(Color.PINK);
+        canvas.show();
+
     }
 }
