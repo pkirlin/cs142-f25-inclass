8f308d3bd8e1756dd7c91bd9aff344adbd6a7da3
Phil Kirlin <kirlinp@rhodes.edu>
2025-09-19 13:11:33 -0500

    updates

--- src/oop2/Dog.java
+++ src/oop2/Dog.java
@@ -3,13 +3,57 @@ package oop2;
 public class Dog {
     private String name;
     private int age;
+    private int energy;
 
     public Dog(String newName, int newAge) {
         name = newName;
-        age = newAge;
+        setAge(age);
+        energy = 3;
+    }
+
+    public int getEnergy() {
+        return energy;
+    }
+
+    public void setEnergy(int energy) {
+        if (energy > 0) {
+            this.energy = energy;
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int newAge) {
+        if (newAge >= 0) {
+            age = newAge;
+        }
+    }
+
+    public void chase(Dog otherdog) {
+        // print out the name of our dog and the name of the other dog
+        System.out.println(getName() + " is chasing " + otherdog.getName());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public void speak() {
         System.out.println(name + " says woof woof");
+    }
+
+    public void playFetch() {
+        System.out.println(name + " is playing fetch.");
+        energy--;
+    }
+
+    public String toString() {
+        return "Name = " + name + ", age = " + age + ", energy = " + energy;
     }
 }
--- src/oop2/DogDemo.java
+++ src/oop2/DogDemo.java
@@ -2,15 +2,24 @@ package oop2;
 
 public class DogDemo {
     public static void main(String[] args) {
-        Dog fido = new Dog("Fido", 5);
+        Dog fido = new Dog("Fido", -5);
         Dog toto = new Dog("Toto", 7);
         //fido.name = "Fido";
         //toto.name = "Toto";
         //fido.age = 5;
         //toto.age = 7;
         //System.out.println("Toto's age is " + toto.age);
-
+        fido.setAge(-6);
+        System.out.println("Fido's age is now " + fido.getAge());
+        System.out.println(fido);
+        System.out.println(toto);
         fido.speak();
         toto.speak();
+        fido.chase(toto);
+        toto.chase(fido);
+        fido.chase(fido);
+        System.out.println(toto);
+        toto.playFetch();
+        System.out.println(toto);
     }
 }
