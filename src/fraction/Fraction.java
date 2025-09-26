627478b866ecedf0c3f1ae4fc9ea2e7c515ca281
Phil Kirlin <kirlinp@rhodes.edu>
2025-09-26 16:23:40 -0500

    updates

--- src/fraction/Fraction.java
+++ src/fraction/Fraction.java
@@ -1,5 +1,36 @@
 package fraction;
 
 public class Fraction {
+    private int numer, denom;
 
+    public Fraction(int newNumer, int newDenom) {
+        numer = newNumer;
+        denom = newDenom;
+        if (denom < 0) {
+            numer = -numer;
+            denom = -denom;
+        }
+        if (denom == 0) {
+            System.out.println("can't divide by zero");
+            System.exit(1);
+        }
+        reduce();
+    }
+
+    private void reduce() {
+        int gcd = ExtraMath.gcd(numer, denom);
+        numer = numer / gcd;
+        denom = denom / gcd;
+    }
+
+    public String toString() {
+        return numer + "/" + denom;
+    }
+
+    public Fraction add(Fraction otherFraction) {
+        int newNumer = numer * otherFraction.denom + denom * otherFraction.numer;
+        int newDenom = denom * otherFraction.denom;
+        Fraction answer = new Fraction(newNumer, newDenom);
+        return answer;
+    }
 }
--- src/fraction/FractionDemo.java
+++ src/fraction/FractionDemo.java
@@ -4,6 +4,12 @@ public class FractionDemo {
     public static void main(String[] args) {
         // Write code here to demonstrate and test the functionality of your fraction class!
 
-
+        Fraction f1 = new Fraction(1, 2);
+        Fraction f2 = new Fraction(4, 6);
+        System.out.println(f1);
+        System.out.println(f2);
+        System.out.println(f1.add(f2));
+        //f2.reduce();
+        //System.out.println(f2);
     }
 }
