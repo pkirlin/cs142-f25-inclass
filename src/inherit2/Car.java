4ddf7c71f661a0c705dc8c9a639fff503fa2c356
Phil Kirlin <kirlinp@rhodes.edu>
2025-11-07 10:30:37 -0600

    updates

--- src/inherit2/Car.java
+++ src/inherit2/Car.java
@@ -11,7 +11,7 @@ public class Car {
         totalDistanceTraveled = 0;
     };
 
-    public double getDistance() {
+    public double getDistance()  {
         return totalDistanceTraveled;
     }
 
